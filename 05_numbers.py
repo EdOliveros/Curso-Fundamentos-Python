9746df705acb8842c2de76448f3cc9cb99b9bbf9
Ed Oliveros <edgarolimoy98@gmail.com>
2023-02-26 12:56:06 -0500

    Booleans

---
+++ 05_numbers.py
@@ -0,0 +1,45 @@
+lives = 3
+print(type(lives))
+
+age = 12
+budget = 100
+
+temperature = 12.12
+print(type(temperature))
+
+lives = 2
+print(lives)
+
+lives = 1
+print(lives)
+
+lives = 12 + 4 
+print(lives)
+
+lives = lives -1
+print(lives)
+
+lives -= 1
+print(lives)
+
+lives -= 5
+print(lives)
+
+lives += 5
+print(lives)
+
+number = 45000000000000000000.5
+print(number)
+
+number_b = 0.000000000000000000000000000000000000000001
+print(number_b)
+
+enero = 100
+febrero = 200
+marzo = 900
+print("enero: ", enero,"febrero: ", febrero , "marzo: ", marzo)
+
+total_meses = enero + febrero + marzo
+
+promedio = total_meses / 3
+print("promedio: ", promedio)
